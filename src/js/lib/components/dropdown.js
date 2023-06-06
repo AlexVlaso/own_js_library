1e3d59d470037b5409946b2261159ee69ed06d18
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-06-06 17:45:34 +0300

    add modal component

--- src/js/lib/components/dropdown.js
+++ src/js/lib/components/dropdown.js
@@ -4,7 +4,8 @@ $.prototype.dropdown = function () {
   for (let i = 0; i < this.length; i++) {
     const id = this[i].getAttribute("id");
     const menu = document.querySelector(`[data-dropdown]=${id}`);
-    this[i].click(() => {
+    $(this[i]).click((e) => {
+      e.preventDefault();
       menu.style.display = "";
     });
   }
